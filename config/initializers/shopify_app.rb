b87a8a422db4abfe8641d202b769f2162787c544
NikhilPeri <nikhil.peri@shopify.com>
2017-11-06 23:42:23 -0500

    add api keys

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.application_name = "Booking"
+  config.api_key = ENV["SHOPIFY_PUBLIC_KEY"]
+  config.secret = ENV["SHOPIFY_SECRET_KEY"]
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
