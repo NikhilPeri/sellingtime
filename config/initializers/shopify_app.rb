f249335660452ca2353b19b86373177610963d8f
NikhilPeri <nikhil.peri@shopify.com>
2017-11-26 23:19:39 -0500

    skeleton webhooks

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,8 +2,14 @@ ShopifyApp.configure do |config|
   config.application_name = "Booking"
   config.api_key = ENV["SHOPIFY_PUBLIC_KEY"]
   config.secret = ENV["SHOPIFY_SECRET_KEY"]
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, write_products"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
+  config.webhooks = [
+    {topic: 'orders/paid', address: 'http://7def1638.ngrok.io/webhooks/orders_paid'},
+    {topic: 'checkouts/create', address: 'http://7def1638.ngrok.io/webhooks/checkouts_create'},
+    {topic: 'checkouts/delete', address: 'http://7def1638.ngrok.io/webhooks/checkouts_delete'},
+    {topic: 'checkouts/update', address: 'http://7def1638.ngrok.io/webhooks/checkouts_update'},
+  ]
 end
---
+++ test/jobs/checkouts_delete_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CheckoutsDeleteJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
